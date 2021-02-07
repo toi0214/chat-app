f6a9c42668da77b820656d4e2e34fbbcca0b7fef
toi0214 <78341100+toi0214@users.noreply.github.com>
2021-02-08 00:04:09 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "messages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
