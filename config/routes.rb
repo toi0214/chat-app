6c10a2344ff78007564468965a15896cad135aaf
toi0214 <78341100+toi0214@users.noreply.github.com>
2021-02-08 16:52:33 +0900

    チャットルーム作成機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "rooms#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
