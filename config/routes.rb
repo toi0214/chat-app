d19f85d629a1e6cf18e07c1575b01558dcd86089
toi0214 <78341100+toi0214@users.noreply.github.com>
2021-02-08 18:19:33 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
-    resources :messages, only: [:index, :create]
-  end
+  resources :rooms, only: [:new, :create]
 end
